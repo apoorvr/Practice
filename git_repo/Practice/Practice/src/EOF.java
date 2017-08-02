40f13a9149c4944fc6a3d243b342c8b911fdbdad
Apoorv Rastogi <apoorvr@vmware.com>
2017-08-02 17:03:11 +0530

    initial commit of sample code for java

---
+++ git_repo/Practice/Practice/src/EOF.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class EOF {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner ( System.in );
+        int i =1 ;
+        while (sc.hasNext()){
+            System.out.print(i+++" ");
+            System.out.println(sc.nextLine()) ;
+        }
+    }
+
+}
